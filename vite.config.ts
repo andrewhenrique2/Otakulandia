83a7c629edea70f797e44d50fa435030d1b941a4
andrewhenrique2 <andrewloly06@gmail.com>
2024-06-07 00:27:06 -0300

    Update Vite configuration for react-toastify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000', // Redireciona as requisições que começam com '/api' para o backend
     },
   },
+  resolve: {
+    alias: {
+      'react-toastify': require.resolve('react-toastify')
+    }
+  }
 });
